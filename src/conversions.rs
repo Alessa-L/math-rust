d29ad9e72a38e2494d09df7c2bb9f0918b5e14fe
Alessa L <alessa.code.fnrcz@slmail.me>
2023-06-16 10:08:47 -0300

    finished some stuff and got frustrated with graphs

--- src/conversions.rs
+++ src/conversions.rs
@@ -8,7 +8,7 @@ use std::{
 // Binary to decimal: Vec[digit * 2.pow(n)]
 // bit shift could also be used in this instance: Vec[digit << n]
 
-pub fn get_input() -> Vec<u8> {
+pub fn get_input(radix: u8) -> Vec<u8> {
     let mut user_input: String = String::new();
     print!("Please input a number: ");
     io::stdout().flush().unwrap(); // The stdout needs to be flushed to ensure subsequent output is immediate.
@@ -17,7 +17,7 @@ pub fn get_input() -> Vec<u8> {
         Err(error) => {
             eprintln!("Error: {error}");
             println!("Let's try this again.");
-            return get_input()
+            return get_input(radix)
         }
     }
 
@@ -27,7 +27,7 @@ pub fn get_input() -> Vec<u8> {
         if digit.is_whitespace() {
             continue
         }
-        match parse_input(digit, 2) { // Try to transform the character into a integer digit of the specified radix.
+        match parse_input(digit, radix) { // Try to transform the character into a integer digit of the specified radix.
             Some(digit) => { binary_number.push(digit) }
             None => {
                 eprintln!("Invalid number!");
@@ -39,21 +39,29 @@ pub fn get_input() -> Vec<u8> {
     return binary_number
 }
 
-pub fn parse_input(char_digit: char, radix: u8) -> Option<u8> { 
+fn parse_input(char_digit: char, radix: u8) -> Option<u8> { 
     let mut digits: Vec<(char, u8)> = (0 .. radix).map(|i| (i.to_string().chars().nth(0).unwrap(), i) ).collect();
-    if radix > 9 {
+    if radix > 10 {
         let hexadecimal: Vec<(char, u8)> = (10 .. 16).map(|i| (
-            ('a' .. 'f').nth(i - 10).unwrap(),
+            ('A' .. 'F').nth(i - 10).unwrap(),
             i as u8) ).collect();
         digits.extend(hexadecimal);
     }
     let digits_map: HashMap<char, u8> = HashMap::from_iter(digits);
-    
+
     if digits_map.contains_key(&char_digit) {
-        return Some(digits_map[&char_digit])
+        return Some( digits_map[&char_digit] )
     } else {
         return None
     }
+}
+
+pub fn binary_dec(binary_number: &mut Vec<u8>) -> u32 {
+    let mut decimal_number: u32 = 0;
+    for (i, n) in binary_number.into_iter().enumerate() {
+        decimal_number += (*n as u32) << i;
+    }
+    return decimal_number
 }
 
 pub fn binary_hex(binary_number: &mut Vec<u8>) -> Vec<String> {
@@ -87,11 +95,22 @@ pub fn binary_hex(binary_number: &mut Vec<u8>) -> Vec<String> {
         let digit: usize = digit.parse().unwrap();
         if digit > 9 {
             hex_calc[i] = hex_digits[&digit].try_into().unwrap();
-        }
-        else {
+        } else {
             hex_calc[i] = digit.to_string();
         }
     }
 
     return hex_calc
+}
+
+pub fn decimal_bin(decimal_number: &mut f32) -> Vec<u8> {
+    let mut binary_number: Vec<u8> = vec![];
+    while *decimal_number != 1.0 {
+        let remainder: u8 = (*decimal_number % 2.0) as u8;
+        *decimal_number -= (*decimal_number / 2.0).ceil(); // WHY DOES CEIL() RETURN THE SMALLEST INTEGER!!!!!!!!!!
+        binary_number.push(remainder);
+    }
+    binary_number.push(1);
+    binary_number.reverse();
+    return binary_number;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::io::stdin;
+use std::{io::stdin, process::exit};
+use conversions::get_input;
 
 mod conversions;
-mod graph;
 fn main() {
     let options: [&str; 6] = [
         "binary to decimal conversions.",
@@ -23,20 +23,43 @@ fn main() {
         Err(error) => { eprintln!("{error}"); }
     }
     match input.as_str() {
-        "1" => println!("Binary to decimal."),
+        "1" => {
+            println!("Binary to decimal.");
+            let mut binary_number: Vec<u8> = vec![];
+            while binary_number.len() == 0 {
+                binary_number = conversions::get_input(2);
+            }
+            let dec_number: u32 = conversions::binary_dec(&mut binary_number);
+            println!("Your number in hexadecimal is: {}", dec_number);
+        }
         "2" => {
             println!("Binary to hexadecimal.");
-            let mut binary_number: Vec<u8> = conversions::get_input();
+            let mut binary_number: Vec<u8> = vec![];
+            while binary_number.len() == 0 {
+                binary_number = conversions::get_input(2);
+            }
             let hex_number: Vec<String> = conversions::binary_hex(&mut binary_number);
             println!("Your number in hexadecimal is: ");
             for i in hex_number {
                 print!("{i}");
             }
         }
-        "3" => println!("decimal to binary."),
+        "3" => {
+            println!("decimal to binary.");
+            let decimal_vector: Vec<u8> = get_input(10);
+            // We "unpack" the vector with the appropriate decimal place value
+            let mut decimal_number: f32  = decimal_vector.iter().fold(0.0, |accum, n| (accum * 10.0) + *n as f32);
+            let binary_number: Vec<u8> = conversions::decimal_bin(&mut decimal_number);
+            println!("Your number in binary is: ");
+            for i in binary_number {
+                print!("{}", i);
+            }
+        }
         "4" => println!("decimal to hexadecimal."),
         "5" => println!("hexadecimal to binary."),
         "6" => println!("hexadecimal to decimal."),
         &_ => println!("Invalid option."),
     }
+    
+    exit(0)
 }
