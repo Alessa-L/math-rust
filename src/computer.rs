e42222fd608c6c4f26954a496266ad93ab169da8
Alessa L <alessa.code.fnrcz@slmail.me>
2023-07-04 19:45:34 -0300

    fixed a mistake oopsie

--- src/computer.rs
+++ src/computer.rs
@@ -2,6 +2,7 @@ pub fn bubble_sort() {
     let mut arr: Vec<i32> = vec![9, 5, 6, 7, 8, 2, 4, 7];
     let mut swapped: bool = false;
     for i in 0 .. arr.len() {
+        swapped = false;
             for i in 0 .. (arr.len() - 1) {
                 let n = arr[i];
                 if n > arr[i + 1] {
@@ -10,9 +11,9 @@ pub fn bubble_sort() {
                     swapped = true;
                 }
             }
-            if swapped == false {
-                break;
-            }
+        if swapped == false {
+            break;
         }
+    }
     println!("{:?}", arr)
 }
